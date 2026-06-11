9c36902ba06dccccca5e6b264fb0ec3b8636d666
Ayan <2ayanagarwal09@gmail.com>
2026-06-10 18:09:15 -0600

    hello world

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    //standard character output = std basically sys.out.print
+    //endl is end line or \n
+    std::cout << "hello" <<std::endl;
+    std::cout << 19 <<'\n';
+    return 0;
+}
